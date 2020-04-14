00b6904bcec6ccfd78ff6714cfbfd6af9307a55e
Ermias Hailemicheal <ermias.hailemicheal@frantic.com>
2020-04-14 18:49:06 +0300

    Test husky

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+const a = 'A';
 ReactDOM.render(
   <StrictMode>
     <App />
