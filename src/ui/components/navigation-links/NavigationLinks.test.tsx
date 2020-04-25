b18e5967050e14d8e78dbf7bf38936b24db61bce
Timo Turkki <timo.turkki@frantic.com>
2020-04-25 14:19:06 +0300

    clean up tests

--- src/ui/components/navigation-links/NavigationLinks.test.tsx
+++ src/ui/components/navigation-links/NavigationLinks.test.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import NavigationLinks from './NavigationLinks';
-
-const renderNavInRouter = (): React.ReactElement => {
-  return (
-    <Router>
-      <NavigationLinks />
-    </Router>
-  );
-};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(renderNavInRouter(), div);
+  ReactDOM.render(<NavigationLinks />, div);
 });
--- src/ui/components/navigation/Navigation.test.tsx
+++ src/ui/components/navigation/Navigation.test.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import Navigation from './Navigation';
-
-const renderNavInRouter = (): React.ReactElement => {
-  return (
-    <Router>
-      <Navigation />
-    </Router>
-  );
-};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(renderNavInRouter(), div);
+  ReactDOM.render(<Navigation />, div);
 });
